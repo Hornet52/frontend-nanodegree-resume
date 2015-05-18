0a89e0ccb24ac811c8dc5fa52ae5fac82f80fb1c
Kyle Hurtley <kyle@kylehurtley.com>
2015-05-18 16:06:02 -0400

    Finish lesson 2

---
+++ js/resumeBuilder.js
@@ -0,0 +1,69 @@
+//$("#main").append(funThoughts);
+
+var work = {
+	"jobs": [
+		{
+			"employer": "PDS Tech, Inc.",
+			"title": "Account Associate",
+			"location": "Indianapolis, IN",
+			"dates": "1/2015 - present",
+			"description": "blah blah blah blah"
+		}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title": "project 1",
+			"dates": "2015",
+			"description": "blah blah blah",
+			"images": "http://placehold.it/200x200"
+		}
+	]
+}
+
+var bio = {
+	"name": "Kyle Hurtley",
+	"role": "Web Developer",
+	"welcomeMessage": "OMG HI!",
+	"skills": ["HTML", "Javascript", "Bootstrap"],
+	"contacts": [
+		{
+			"mobile": "317-340-1076",
+			"email": "kyle@kylehurtley.com",
+			"github": "hornet52",
+			"twitter": "indyskeptic",
+			"location": "Fishers, IN"
+		}
+	]
+}
+
+var education = {
+	"schools": [
+		{
+			"name": "Noblesville High School",
+			"location": "Noblesville, IN",
+			"degree": "N/A",
+			"majors": ["N/A"],
+			"dates": "1995 - 1999",
+			"URL": "http://www.noblesvilleschools.org/Domain/8"
+		},
+		{
+			"name": "Ivy Tech Community College",
+			"location": "Indianapolis, IN",
+			"degree": "None",
+			"majors": ["Computer Information Technology"],
+			"dates" : "2009 - 2011",
+			"URL": "http://www.ivytech.edu"
+		}
+	],
+	"onlineCourses": [
+			{
+				"title": "JavaScript Basics",
+				"school": "Udacity",
+				"dates": "5/2015",
+				"URL": "https://www.udacity.com/course/ud804"
+			}
+	]
+}
